c9452041699ed6b68d0b4dc7a0038d533b095de7
trodat3 <joaopedroabrantescarvalho@gmail.com>
2025-09-23 12:16:27 -0300

    Alterando App.tsx para poder utilizar os componentes criados

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Navegacao from './components/Navegacao.tsx';
+import PaginaAnimais from './pages/Animais.tsx';
+import PaginaLivros from './pages/Livros.tsx';
+import PaginaPessoas from './pages/Pessoas.tsx';
+import PaginaFavoritos from './pages/Favoritos.tsx';
+import './App.css';
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      <div>
-        
+    <div className="container-app">
+      <Navegacao />
+      <div className="conteudo">
+        <Routes>
+          <Route path="/" element={<Navigate to="/animais" />} />
+          <Route path="/animais" element={<PaginaAnimais />} />
+          <Route path="/livros" element={<PaginaLivros />} />
+          <Route path="/pessoas" element={<PaginaPessoas />} />
+          <Route path="/favoritos" element={<PaginaFavoritos />} />
+        </Routes>
       </div>
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
